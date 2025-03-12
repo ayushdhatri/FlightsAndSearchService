d929f722cb0bb402a87231a170b06070ddcdcd65
Ayush Solanki <aayushdhatri@gmail.com>
2025-03-12 20:49:17 +0530

    basic setup done for database using sequelize

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const setupAndStartServer = async() => {
     // create the express object
 
     const app = express();
+    // to manipulate the data easily by server we convert it into javascript object
     app.use(bodyParser.json());
+    //When the user submits this form, the browser sends the form data to the server in a URL-encoded format, like this:
     app.use(bodyParser.urlencoded({extended:true}));
   
     app.listen(PORT, ()=>{
