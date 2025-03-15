0ce34bea47761c5629ee0203b09b64c91238ffc2
Ayush Solanki <aayushdhatri@gmail.com>
2025-03-15 13:21:04 +0530

    added crud operations in city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,17 +1,19 @@
 const { City } = require('../models/index');// will get the corresponding models from models folder
 
 class CityRepository {
+    // function to createCity and insert in db
     async createCity( {name}) {
         try {
             const city = await City.create({name}); 
             return city;
         }
         catch(error) {
+            console.log("Something went wrotn in the repository layer");
             throw {error};
 
         }
     }
-
+    // function to delete a tuble city from db
     async deleteCity({ cityId }) {
         try {
             await City.destroy({
@@ -22,9 +24,37 @@ class CityRepository {
 
         }
         catch(error) {
+            console.log("Something went wrotn in the repository layer");
             throw {error};
         }
     }
+    // function to update the city
+    async updateCity({cityId}){
+        try {
+            const city = await City.update(data, {
+                where : {
+                    id : cityId
+                }
+            });
+
+        }
+        catch(error) {
+            throw {error};
+        }
+    }
+
+    // function to getCity
+    async getCity({cityId}){
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        }
+        catch(error) {
+            throw {error};
+        }
+    }
+
+
 }
 
 module.exports = CityRepository;
